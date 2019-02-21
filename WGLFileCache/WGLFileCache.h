316b25d4bdab252d2729039cbd184c7961fc2e91
wuguanlong <guanlongwu@qq.com>
2019-02-21 16:34:22 +0800

    update file cache

---
+++ WGLFileCache/WGLFileCache.h
@@ -0,0 +1,13 @@
+//
+//  WGLFileCache.h
+//  WGLFileCache
+//
+//  Created by wugl on 2019/2/21.
+//  Copyright © 2019年 WGLKit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WGLFileCache : NSObject
+
+@end
---
+++ WGLFileCache/WGLFileCache.m
@@ -0,0 +1,13 @@
+//
+//  WGLFileCache.m
+//  WGLFileCache
+//
+//  Created by wugl on 2019/2/21.
+//  Copyright © 2019年 WGLKit. All rights reserved.
+//
+
+#import "WGLFileCache.h"
+
+@implementation WGLFileCache
+
+@end
